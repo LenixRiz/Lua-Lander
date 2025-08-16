baeda31ac47a7c54a8a1f287333dc22b8c0d3de9
Rizky Fajar Maulana <rizky.fajarbusiness945@gmail.com>
2025-08-16 19:03:48 +0700

    Adding Landing Pad, Score function, and Text Mesh
    Adding Landing Pad, Score function, and Text Mesh

--- Assets/Scripts/Lander.cs
+++ Assets/Scripts/Lander.cs
@@ -1,8 +1,14 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Lander : MonoBehaviour
 {
+
+    public event EventHandler OnLeftForce;
+    public event EventHandler OnRightForce;
+    public event EventHandler OnUpForce;
+    public event EventHandler OnBeforeForce;
 
     private Rigidbody2D landerRigidbody2D;
 
@@ -13,27 +19,40 @@ public class Lander : MonoBehaviour
 
     private void FixedUpdate()
     {
+        OnBeforeForce?.Invoke(this, EventArgs.Empty);
+
         if (Keyboard.current.upArrowKey.isPressed)
         {
             float force = 700f;
             landerRigidbody2D.AddForce(force * Time.deltaTime * transform.up);
+            OnUpForce?.Invoke(this, EventArgs.Empty);
         }
         if (Keyboard.current.leftArrowKey.isPressed)
         {
             float turn = +100f;
             landerRigidbody2D.AddTorque(turn * Time.deltaTime);
+            OnLeftForce?.Invoke(this, EventArgs.Empty);
         }
         if (Keyboard.current.rightArrowKey.isPressed)
         {
             float turn = -100f;
             landerRigidbody2D.AddTorque(turn * Time.deltaTime);
+            OnRightForce?.Invoke(this, EventArgs.Empty);
+        
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (!collision.gameObject.TryGetComponent(out LandingPad landingPad))
+        {
+            Debug.Log("Crashed on terrain!");
+            return;
+        }
+
         float softLandingVelocityMagnitude = 3f;
-        if (collision.relativeVelocity.magnitude > softLandingVelocityMagnitude)
+        float relativeVelocityMagnitude = collision.relativeVelocity.magnitude;
+        if (relativeVelocityMagnitude > softLandingVelocityMagnitude)
         {
             Debug.Log("Landing too hard!");
             return; //No need to run the below
@@ -49,6 +68,19 @@ public class Lander : MonoBehaviour
         }
 
         Debug.Log("Successfully Landing!");
+
+        float maxScoreAmountLandingAngle = 100;
+        float scoreDotVectorMultiplier = 10f;
+        float landingAngleScore = maxScoreAmountLandingAngle - Mathf.Abs(dotVector - 1f) * scoreDotVectorMultiplier * maxScoreAmountLandingAngle;
+
+        float maxScoreAmountLandingSpeed = 100;
+        float landingSpeedScore = (softLandingVelocityMagnitude - relativeVelocityMagnitude) * maxScoreAmountLandingSpeed;
+
+        int finalScore = Mathf.RoundToInt((landingAngleScore + landingSpeedScore) * landingPad.getScoreMultiplier());
+
+        Debug.Log($"Landing Angle Score: {landingAngleScore}");
+        Debug.Log($"Landing Speed Score: {landingSpeedScore}");
+        Debug.Log($"Landing Speed Score: {finalScore}");
 
     }
 
